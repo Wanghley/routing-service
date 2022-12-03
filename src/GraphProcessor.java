a557c2bdab1736de688b0d09343853076fe1bddf
Wanghley Soares Martins <wanghley.soares.martins@duke.edu>
2022-12-03 15:37:11 -0500

    refactor

--- src/GraphProcessor.java
+++ src/GraphProcessor.java
@@ -233,13 +233,13 @@ public class GraphProcessor {
         return path;
     }
 
-    public static void main(String[] args) throws Exception {
-        GraphProcessor gp = new GraphProcessor();
-        gp.initialize(new FileInputStream("data/durham.graph"));
-        // System.out.println(gp.connected(new Point(2.0, -1.0), new Point(-1.0, 1.0)));
-        // System.out.println(gp.route(new Point(47.578813, -122.139773), new Point(47.632292, -122.187898)));
-        // System.out.println(gp.routeDistance(gp.route(new Point(47.578813, -122.139773), new Point(47.632292, -122.187898))));
-        System.out.println(gp.connected(new Point(35.989709, -78.902124), new Point(35.834585, -78.638592)));
-        System.out.println();
-    }
+    // public static void main(String[] args) throws Exception {
+    //     GraphProcessor gp = new GraphProcessor();
+    //     gp.initialize(new FileInputStream("data/durham.graph"));
+    //     // System.out.println(gp.connected(new Point(2.0, -1.0), new Point(-1.0, 1.0)));
+    //     // System.out.println(gp.route(new Point(47.578813, -122.139773), new Point(47.632292, -122.187898)));
+    //     // System.out.println(gp.routeDistance(gp.route(new Point(47.578813, -122.139773), new Point(47.632292, -122.187898))));
+    //     System.out.println(gp.connected(new Point(35.989709, -78.902124), new Point(35.834585, -78.638592)));
+    //     System.out.println();
+    // }
 }
